ad87d1384b239163f3c8f21a06c443dfc802c040
MayarQasrawi <mayar.ya12@gmail.com>
2025-11-26 01:55:09 +0200

    chore: update mobile hook and app routing

---
+++ src/routers.tsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from "react-router-dom";
+import CitiesPage from "@/Pages/admin/cities/index";
+import HotelsPage from "@/Pages/admin/hotels/index";
+import RoomsPage from "@/Pages/admin/rooms/index";
+import LoginPage from "@/Pages/Login/index";
+import AdminLayout from "./components/Layout/AdminLayout";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <LoginPage />,
+	},
+	{
+		path: "admin/",
+		element: <AdminLayout />,
+		children: [
+			{
+				path: "cities",
+				index: true,
+				element: <CitiesPage />,
+			},
+			{
+				path: "hotels",
+				element: <HotelsPage />,
+			},
+			{
+				path: "rooms",
+				element: <RoomsPage />,
+			},
+		],
+	},
+]);
+
+export default router;
