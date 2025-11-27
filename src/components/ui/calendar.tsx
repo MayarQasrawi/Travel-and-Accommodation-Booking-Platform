4f5a8e7f2aec69d27bc2494cdcd7a241236d7016
MayarQasrawi <mayar.ya12@gmail.com>
2025-11-27 23:09:09 +0200

    fix: update Button import and separate buttonVariants

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,8 +1,9 @@
 import { ChevronDownIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import * as React from "react";
 import { type DayButton, DayPicker, getDefaultClassNames } from "react-day-picker";
-import { Button, buttonVariants } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { buttonVariants } from "./button-variants";
 
 function Calendar({
 	className,
