65676dc5efee513137f60673df84734ab7ec01f3
MayarQasrawi <mayar.ya12@gmail.com>
2025-12-18 21:01:39 +0200

    refactor(auth-layout & login): add typing, barrels, and flat imports
    - Strongly type AuthLayout compound components
    - Add barrel index file for AuthLayout exports
    - Re-export subcomponents for direct imports (Brand, Form, FormTitle, Logo, Image, Footer)
    - Update LoginPage to use flat imports for cleaner and more readable JSX

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,18 +1,20 @@
 import type React from "react";
-import AuthLayout from "@/components/Layout/AuthLayout/AuthLayout";
+import { AuthLayout, Brand, Footer, Form, FormTitle, Image, Logo } from "@/components/Layout/AuthLayout";
+
 import LoginForm from "./components/LoginForm";
 
 const LoginPage: React.FC = () => {
 	return (
 		<AuthLayout>
-			<AuthLayout.Brand>
-				<AuthLayout.Logo />
-				<AuthLayout.Animation />
-			</AuthLayout.Brand>
-			<AuthLayout.Form>
-				<AuthLayout.FormTitle title="Sign In" subtitle="Enter your credentials to access your account" />
+			<Brand>
+				<Logo />
+				<Image />
+				<Footer />
+			</Brand>
+			<Form>
+				<FormTitle title="Sign In" subtitle="Enter your credentials to access your account" />
 				<LoginForm />
-			</AuthLayout.Form>
+			</Form>
 		</AuthLayout>
 	);
 };
