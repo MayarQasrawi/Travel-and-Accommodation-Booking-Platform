ec78334e7c95211473c3cb33c599e07167846fe7
MayarQasrawi <mayar.ya12@gmail.com>
2025-12-22 22:56:18 +0200

    feat(booking): add header ffor the page and improve the  layout

--- src/Pages/Booking/Booking.tsx
+++ src/Pages/Booking/Booking.tsx
@@ -1,4 +1,5 @@
 import { Loader } from "@/components/common/Loader";
+import PageHeader from "@/components/common/PageHeader";
 import BookingDetails from "@/Pages/Confirmation/components/BookingDetails";
 import EmptyCart from "./EmptyState";
 import { useGetBookings } from "./hooks/useGetBookings";
@@ -6,15 +7,21 @@ import { useGetBookings } from "./hooks/useGetBookings";
 function Booking() {
 	const { data, isLoading, error } = useGetBookings();
 
-	if (isLoading) return <Loader className="w-32 h-32" />;
+	if (isLoading) return <Loader className="w-32 h-32 mx-auto mt-20" />;
 	if (error || !data || data.length === 0) return <EmptyCart />;
 
 	return (
-		<div className="space-y-4">
-			{data.map((booking) => (
-				<BookingDetails key={booking.confirmationNumber} booking={booking} showDownload />
-			))}
-		</div>
+		<section className="container p-4 md:p-10 min-h-screen justify-center">
+			<PageHeader title="Your Bookings" subtitle="See details of all your confirmed bookings" />
+
+			<main className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+				{data.map((booking) => (
+					<div key={booking.confirmationNumber}>
+						<BookingDetails booking={booking} showDownload />
+					</div>
+				))}
+			</main>
+		</section>
 	);
 }
 
