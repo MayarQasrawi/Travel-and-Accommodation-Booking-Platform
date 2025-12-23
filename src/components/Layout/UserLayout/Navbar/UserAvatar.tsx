dfb6f3bca4cb29bad3c0f61d3fa2338645d432f8
MayarQasrawi <mayar.ya12@gmail.com>
2025-12-23 02:11:07 +0200

    feat: add logout option to the navbar

--- src/components/Layout/UserLayout/Navbar/UserAvatar.tsx
+++ src/components/Layout/UserLayout/Navbar/UserAvatar.tsx
@@ -1,11 +1,32 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { useAuth } from "@/hooks/useAuth";
 
 function UserAvatar() {
+	const { user, logout } = useAuth();
+	const [open, setOpen] = useState(false);
+
 	return (
-		<Avatar className="h-8 w-8" aria-label="User profile">
-			<AvatarImage src="/path-to-user-image.jpg" alt="User avatar" />
-			<AvatarFallback className="bg-gray-300 rounded-full flex items-center justify-center h-8 w-8">U</AvatarFallback>
-		</Avatar>
+		<div className="relative inline-block">
+			<Button className="h-8 w-8 rounded-full p-0" onClick={() => setOpen(!open)} aria-label="User profile">
+				<span className="text-primary-foreground font-semibold">{user?.givenName[0]}</span>
+			</Button>
+
+			{open && (
+				<div className="absolute right-0 mt-2 w-32 bg-white border border-gray-300 rounded shadow-md z-10">
+					<Button
+						variant="ghost"
+						className="w-full justify-start rounded-none px-4 py-2  "
+						onClick={() => {
+							logout();
+							setOpen(false);
+						}}
+					>
+						Logout
+					</Button>
+				</div>
+			)}
+		</div>
 	);
 }
 
